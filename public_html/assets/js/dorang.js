11b14b199c410c5cb9a6cf047fb3835c37258809
szborok <borok.szabolcs@gmail.com>
2024-07-04 18:59:31 +0200

    .

--- public_html/assets/js/dorang.js
+++ public_html/assets/js/dorang.js
@@ -62,25 +62,21 @@ $(document).ready(function(){
 }); 
 
 
-// Open the Modal
 function openModal() {
-  document.getElementById("myModal").style.display = "block";
+  document.getElementById('myModal').style.display = "flex";
 }
 
-// Close the Modal
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+  document.getElementById('myModal').style.display = "none";
 }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -88,18 +84,11 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
